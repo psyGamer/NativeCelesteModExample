78f962ae710d5af6b3f486b87994e1f25d65a425
psyGamer <psygamer21@gmail.com>
2023-10-28 11:44:47 +0200

    Moved arrow rendering to zig

--- MovingBlockEntity.cs
+++ MovingBlockEntity.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace Celeste.Mod.NativeLibraryMod;
 
@@ -28,21 +34,24 @@ public class MovingBlockEntity : Solid {
     [DllImport("NativeLibraryMod")]
     private static extern unsafe void MovingBlockEntity_Update(long id, MovingBlockEntity* _this, Vector2* position, nint _base, nint moveH, nint moveV, nint collideCheckSolid);
     [DllImport("NativeLibraryMod")]
-    private static extern unsafe void MovingBlockEntity_Render(long id);
-    [DllImport("NativeLibraryMod")]
-    private static extern unsafe void MovingBlockEntity_OnDashed(long id);
+    private static extern unsafe void MovingBlockEntity_Render(long id, Vector2 center, List<MTexture>* arrowSprties, nint getListCount, nint indexListDrawCentered);
     [DllImport("NativeLibraryMod")]
     private static extern unsafe DashCollisionResults MovingBlockEntity_OnDashed(long id, Vector2 direction);
 
     private static unsafe T* GetPtr<T>(T obj) => (T*)Unsafe.AsPointer(ref obj);
-    private bool CollideCheckSolid(float x, float y) => CollideCheck<Solid>(new Vector2(x, y));  
+    private static unsafe T* GetPtrRef<T>(ref T obj) => (T*)Unsafe.AsPointer(ref obj);
+    private unsafe ref byte GetPin(object obj) => ref *(byte*)*(void**) Unsafe.AsPointer(ref obj);
+
+    // Wrappers to be able to call them from native code.
+    private bool CollideCheckSolid(float x, float y) => CollideCheck<Solid>(new Vector2(x, y));
 
     public unsafe MovingBlockEntity(Vector2 position, float width, float height)
         : base(position, width, height, safe: false)
     {
         MovingBlockEntity_ctor(ID);
         OnDashCollide = (_, direction) => MovingBlockEntity_OnDashed(ID, direction);
-        // ArrowSprites = GFX.Game.GetAtlasSubtextures("objects/moveBlock/arrow");
+        ArrowSprites = GFX.Game.GetAtlasSubtextures("objects/moveBlock/arrow");
+        ArrowSprites.Add(GFX.Game["objects/moveBlock/x"]);
         //
         // int tilesWidth = (int)(width / 8f), tilesHeight = (int)(height / 8f);
         // for (int x = 0; x < tilesWidth; x++) {
@@ -66,7 +75,7 @@ public class MovingBlockEntity : Solid {
     }
     
     private delegate void MoveDelegate(float move);
-    private unsafe delegate bool CheckSolidDelegate(float x, float y);
+    private delegate bool CheckSolidDelegate(float x, float y);
     public override unsafe void Update()
     {
         fixed (Vector2* pPos = &Position) {
@@ -95,7 +104,21 @@ public class MovingBlockEntity : Solid {
         // FillColor = Color.Lerp(FillColor, Direction != Vector2.Zero ? PressedBgFill : IdleBgFill, 10f * Engine.DeltaTime);
     }
     
-    public override void Render() {
+    private unsafe delegate int ListCountDelegate(List<MTexture>* list);
+    private unsafe delegate void ListIndexDrawCenteredDelegate(List<MTexture>* list, int index, float x, float y);
+    public override unsafe void Render() {
+        fixed (List<MTexture>* pList = &ArrowSprites)
+        {
+            byte a = 0;
+            ref byte pin = ref a;
+            MovingBlockEntity_Render(ID,
+                Center,
+                pList,
+                Marshal.GetFunctionPointerForDelegate<ListCountDelegate>(list => list->Count),
+                Marshal.GetFunctionPointerForDelegate<ListIndexDrawCenteredDelegate>(
+                    (list, index, x, y) => (*list)[index].DrawCentered(new Vector2(x, y))));
+            // handles.ForEach(handle => handle.Free());
+        }
         // Vector2 position = Position;
         // Position += Shake;
         //
@@ -105,7 +128,7 @@ public class MovingBlockEntity : Solid {
         // Draw.Rect(Center.X - 4f, Center.Y - 4f, 8f, 8f, FillColor);
         //
         // if (Direction != Vector2.Zero)
-        //     ArrowSprites[Calc.Clamp((int) Math.Floor((-(double) Direction.Angle() + Calc.Circle) % Calc.Circle / Calc.Circle * 8.0 + 0.5), 0, 7)].DrawCentered(Center);
+        // ArrowSprites[Calc.Clamp((int) Math.Floor((-(double) Direction.Angle() + Calc.Circle) % Calc.Circle / Calc.Circle * 8.0 + 0.5), 0, 7)].DrawCentered(Center);
         // else
         //     GFX.Game["objects/moveBlock/x"].DrawCentered(Center);
         //
--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,9 @@ const Vec2 = extern struct {
     pub fn add(self: Vec2, other: Vec2) Vec2 {
         return .{ .x = self.x + other.x, .y = self.y + other.y };
     }
+    pub fn angle(self: Vec2) f32 {
+        return std.math.atan2(f32, self.y, self.x);
+    }
 };
 
 const DashCollisionResult = enum(u8) { rebound, normal_collision, normal_override, bounce, ignore };
@@ -31,6 +34,9 @@ pub const MovingBlockEntity = struct {
     pub const PfnMoveH = *const fn (this: *anyopaque, move_h: f32) void;
     pub const PfnMoveV = *const fn (this: *anyopaque, move_v: f32) void;
     pub const PfnCollideCheckSolid = *const fn (this: *anyopaque, x: f32, y: f32) bool;
+    pub const PfnGetAtlasSubtextures = *const fn (this: *anyopaque, key: []const u8) *anyopaque;
+    pub const PfnGetListCount = *const fn (this: *anyopaque) i32;
+    pub const PfnListIndexDrawCentered = *const fn (this: *anyopaque, index: i32, x: f32, y: f32) void;
 
     const idle_bg_fill = Color.fromHex("474070");
     const moving_bg_fill = Color.fromHex("30b335");
@@ -47,7 +53,15 @@ pub const MovingBlockEntity = struct {
         _ = self;
     }
 
-    fn update(self: *Self, this: *anyopaque, position: *Vec2, base: PfnUpdate, move_h: PfnMoveH, move_v: PfnMoveV, collide_check_solid: PfnCollideCheckSolid) void {
+    fn update(
+        self: *Self,
+        this: *anyopaque,
+        position: *Vec2,
+        base: PfnUpdate,
+        move_h: PfnMoveH,
+        move_v: PfnMoveV,
+        collide_check_solid: PfnCollideCheckSolid,
+    ) void {
         base(this);
 
         if (!collide_check_solid(this, position.x + self.direction.x, position.y)) {
@@ -61,10 +75,24 @@ pub const MovingBlockEntity = struct {
             self.direction.y *= -1;
         }
     }
-    fn render(self: *Self, this: *anyopaque, base: PfnUpdate) void {
-        _ = base;
-        _ = this;
-        _ = self;
+    fn render(
+        self: *Self,
+        center: Vec2,
+        arrow_sprites: *anyopaque,
+        get_list_count: PfnGetListCount,
+        list_index_draw_centered: MovingBlockEntity.PfnListIndexDrawCentered,
+    ) void {
+        _ = get_list_count;
+
+        // const count = get_list_count(arrow_sprites);
+        // for (0..@intCast(count)) |i| {
+        //     list_index_draw_centered(arrow_sprites, @intCast(i), center.x, center.y);
+        // }
+        if (self.direction.x != 0 or self.direction.y != 0) {
+            list_index_draw_centered(arrow_sprites, std.math.clamp(@as(i32, @intFromFloat(@floor(@mod(-self.direction.angle() + std.math.tau, std.math.tau) / std.math.tau * 8.0 + 0.5))), 0, 7), center.x, center.y);
+        } else {
+            list_index_draw_centered(arrow_sprites, 8, center.x, center.y);
+        }
     }
     fn on_dashed(self: *Self, direction: Vec2) DashCollisionResult {
         self.direction.x += direction.x;
@@ -92,8 +120,14 @@ pub export fn MovingBlockEntity_Update(
 ) void {
     MovingBlockEntity.entities.getPtr(id).?.update(this, position, base, move_h, move_v, collide_check_solid);
 }
-pub export fn MovingBlockEntity_Render(id: EntityID, this: *anyopaque, base: MovingBlockEntity.PfnUpdate) void {
-    MovingBlockEntity.entities.getPtr(id).?.render(this, base);
+pub export fn MovingBlockEntity_Render(
+    id: EntityID,
+    center: Vec2,
+    arrow_sprites: *anyopaque,
+    get_list_count: MovingBlockEntity.PfnGetListCount,
+    list_index_draw_centered: MovingBlockEntity.PfnListIndexDrawCentered,
+) void {
+    MovingBlockEntity.entities.getPtr(id).?.render(center, arrow_sprites, get_list_count, list_index_draw_centered);
 }
 pub export fn MovingBlockEntity_OnDashed(id: EntityID, direction: Vec2) DashCollisionResult {
     return MovingBlockEntity.entities.getPtr(id).?.on_dashed(direction);
